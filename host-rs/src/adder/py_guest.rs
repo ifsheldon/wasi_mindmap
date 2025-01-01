2d1f3408721eda2ad3f4e387c79c67cc27595957
maple@max <feng.liang@kaust.edu.sa>
2025-01-01 12:12:27 +0800

    add tests

--- host-rs/src/adder/py_guest.rs
+++ host-rs/src/adder/py_guest.rs
@@ -3,3 +3,23 @@ mod sync_version;
 
 pub use async_version::run_adder_async as run_adder_py_async;
 pub use sync_version::run_adder_sync as run_adder_py_sync;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use wasmtime::{Config, Engine, Result};
+
+    #[test]
+    fn test_adder_py_async() -> Result<()> {
+        let mut config = Config::default();
+        config.async_support(true);
+        let engine_async = Engine::new(&config)?;
+        run_adder_py_async(&engine_async)
+    }
+
+    #[test]
+    fn test_adder_py_sync() -> Result<()> {
+        let engine = Engine::default();
+        run_adder_py_sync(&engine)
+    }
+}
--- host-rs/src/adder/rs_guest.rs
+++ host-rs/src/adder/rs_guest.rs
@@ -7,3 +7,34 @@ pub use async_version::run_adder_async as run_adder_rs_async;
 pub use interfaced_sync_version::run_adder_dynamic as run_interfaced_adder_dynamic;
 pub use interfaced_sync_version::run_adder_sync as run_interfaced_adder_sync;
 pub use sync_version::run_adder_sync as run_adder_rs_sync;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use wasmtime::{Config, Engine, Result};
+    #[test]
+    fn test_adder_rs_sync() -> Result<()> {
+        let engine = Engine::default();
+        run_adder_rs_sync(&engine)
+    }
+
+    #[test]
+    fn test_adder_rs_async() -> Result<()> {
+        let mut config = Config::default();
+        config.async_support(true);
+        let engine_async = Engine::new(&config)?;
+        run_adder_rs_async(&engine_async)
+    }
+
+    #[test]
+    fn test_interfaced_adder_dynamic() -> Result<()> {
+        let engine = Engine::default();
+        run_interfaced_adder_dynamic(&engine)
+    }
+
+    #[test]
+    fn test_interfaced_adder_sync() -> Result<()> {
+        let engine = Engine::default();
+        run_interfaced_adder_sync(&engine)
+    }
+}
