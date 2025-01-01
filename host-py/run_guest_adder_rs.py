fc74a78ada7a6440d444cf9e195c83741579c3c8
maple@max <feng.liang@kaust.edu.sa>
2025-01-01 12:04:35 +0800

    add main functions

--- host-py/run_guest_adder_rs.py
+++ host-py/run_guest_adder_rs.py
@@ -1,10 +1,15 @@
 from wasmtime import Store
+
 from adder_rs_bindings import Root
 
 
 def run_adder_rs_guest():
     store = Store()
     adder_component_instance = Root(store)
-    result = adder_component_instance.add(store,1, 2)
+    result = adder_component_instance.add(store, 1, 2)
     assert result == 3
     print(f"{__name__}: 1 + 2 = {result}")
+
+
+if __name__ == "__main__":
+    run_adder_rs_guest()
--- host-py/run_guest_adder_rs_magic_loader.py
+++ host-py/run_guest_adder_rs_magic_loader.py
@@ -4,9 +4,14 @@ from wasmtime import Store
 import wasmtime.loader
 from guest_adder_rs import Root
 
+
 def run_adder_rs_guest():
     store = Store()
     adder_component_instance = Root(store)
     result = adder_component_instance.add(store, 1, 2)
     print(f"{__name__}: 1 + 2 = {result}")
     assert result == 3
+
+
+if __name__ == "__main__":
+    run_adder_rs_guest()
