9817ce733a1bc66a090741077eed510e34927100
ifsheldon <feng.liang@kaust.edu.sa>
2025-06-29 16:00:01 +0800

    fixed a bug

--- guest-kv-store-rs/src/lib.rs
+++ guest-kv-store-rs/src/lib.rs
@@ -4,13 +4,16 @@ wit_bindgen::generate!({
 });
 
 struct KVStore;
+use crate::wasi_mindmap::kv_store::kvdb::Connection;
+use std::sync::LazyLock;
+
+static KV_CONNECTION: LazyLock<Connection> = LazyLock::new(|| Connection::new());
 
 impl Guest for KVStore {
     fn replace_value(key: String, value: String) -> Option<String> {
-        let kv = wasi_mindmap::kv_store::kvdb::Connection::new();
         // replace
-        let old = kv.get(&key);
-        kv.set(&key, &value);
+        let old = KV_CONNECTION.get(&key);
+        KV_CONNECTION.set(&key, &value);
         old
     }
 }
